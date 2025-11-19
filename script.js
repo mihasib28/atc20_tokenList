14303804a2522b2e55ad6bc62c198511d77b1a8c
mihasib <mihasib123@gmail.com>
2025-11-19 18:38:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,284 @@
+// Token list (edit freely)
+const tokens = [
+    {
+    name: "Queen",
+    address: "ACtPnTY6cCqdPWTfvFPJ4mDRAPTyDa1xfruVR41E1SG4",
+    logo: "https://tokenlist.atcscan.io/metadata/ACtPnTY6cCqdPWTfvFPJ4mDRAPTyDa1xfruVR41E1SG4/logo.png"
+},
+{
+    name: "Emperor",
+    address: "ACFTFdoB2woJhqh2NEDLQAxSCFg7kQuJJAiGHpuJ4WYY",
+    logo: "https://tokenlist.atcscan.io/metadata/ACFTFdoB2woJhqh2NEDLQAxSCFg7kQuJJAiGHpuJ4WYY/logo.png"
+},
+{
+    name: "KING",
+    address: "ACYAht7qi27gMNU7ZzTXzy2RTNVZKnfspvzuM5MF7Anq",
+    logo: "https://tokenlist.atcscan.io/metadata/ACYAht7qi27gMNU7ZzTXzy2RTNVZKnfspvzuM5MF7Anq/logo.png"
+},
+{
+    name: "MAX",
+    address: "AC8dLeddJKEyha842ZQiRLLooysrVeB9NVsV7hMwRKad",
+    logo: "https://tokenlist.atcscan.io/metadata/AC8dLeddJKEyha842ZQiRLLooysrVeB9NVsV7hMwRKad/logo.png"
+},
+{
+    name: "DOGE",
+    address: "AChqa1LZ9sFQtLfPhNsPrcMkXGFkgfhnw2bb3TjbdW9V",
+    logo: "https://tokenlist.atcscan.io/metadata/AChqa1LZ9sFQtLfPhNsPrcMkXGFkgfhnw2bb3TjbdW9V/logo.jpg"
+},
+{
+    name: "SOL",
+    address: "ACkAVQaCdvb3rqT1GsGNEqM5oimdw5jvbKc6Dz1QaBaT",
+    logo: "https://tokenlist.atcscan.io/metadata/ACkAVQaCdvb3rqT1GsGNEqM5oimdw5jvbKc6Dz1QaBaT/logo.png"
+},
+{
+    name: "ETH",
+    address: "ACY7LgNL5QV2SHKiAoYRvBAvEaZfLG4mBoUyYW5kjrcY",
+    logo: "https://tokenlist.atcscan.io/metadata/ACY7LgNL5QV2SHKiAoYRvBAvEaZfLG4mBoUyYW5kjrcY/logo.png"
+},
+{
+    name: "BTC",
+    address: "ACGdPai5V3wgtgbB1iG3rpzMpBCaNyZofyqXPvQ45h7r",
+    logo: "https://tokenlist.atcscan.io/metadata/ACGdPai5V3wgtgbB1iG3rpzMpBCaNyZofyqXPvQ45h7r/logo.png"
+},
+{
+    name: "CNYC",
+    address: "ACQqeKkAbnZgp4G8aZsUxQ3We4kgGa55XG1HvsfkqnZR",
+    logo: "https://tokenlist.atcscan.io/metadata/ACQqeKkAbnZgp4G8aZsUxQ3We4kgGa55XG1HvsfkqnZR/logo.png"
+},
+{
+    name: "USDA",
+    address: "AC3ZNwL2dMZGj7thrARcPvZCYGjuTD5C9V8KXdBcCiqQ",
+    logo: "https://tokenlist.atcscan.io/metadata/AC3ZNwL2dMZGj7thrARcPvZCYGjuTD5C9V8KXdBcCiqQ/logo.png"
+},
+{
+    name: "TRX",
+    address: "ACjeBtLQjKvqu8vPW7pa7ik6HPgz2meCyAofYpXMX2Wy",
+    logo: "https://tokenlist.atcscan.io/metadata/ACjeBtLQjKvqu8vPW7pa7ik6HPgz2meCyAofYpXMX2Wy/logo.png"
+},
+{
+    name: "BNB",
+    address: "AC8wvafPjCdtC1L38Y1vkQiQRKVQQgfm1xsiS6AJRpXS",
+    logo: "https://tokenlist.atcscan.io/metadata/AC8wvafPjCdtC1L38Y1vkQiQRKVQQgfm1xsiS6AJRpXS/logo.png"
+},
+{
+    name: "PEPE",
+    address: "AChtEnyeWybwDkMFaiDtWFavne7R3uZ2JCTguQog5kq5",
+    logo: "https://tokenlist.atcscan.io/metadata/AChtEnyeWybwDkMFaiDtWFavne7R3uZ2JCTguQog5kq5/logo.png"
+},
+{
+    name: "KIRK",
+    address: "ACmXEAEZCgi8BHAvgKqzeovTBNfpTZjv5dQypzV3pLjF",
+    logo: "https://tokenlist.atcscan.io/metadata/ACmXEAEZCgi8BHAvgKqzeovTBNfpTZjv5dQypzV3pLjF/logo.png"
+},
+{
+    name: "AzukiCoin",
+    address: "ACuaycuupvSBHZ7ijU7gT6GvSapm81khxa7itVjrsAVP",
+    logo: "https://tokenlist.atcscan.io/metadata/ACuaycuupvSBHZ7ijU7gT6GvSapm81khxa7itVjrsAVP/logo.png"
+},
+{
+    name: "LABUBU",
+    address: "ACXDi4GUfURUSzeEeVtybJ2HKnuhNphovZ6KmS6XLp9R",
+    logo: "https://tokenlist.atcscan.io/metadata/ACXDi4GUfURUSzeEeVtybJ2HKnuhNphovZ6KmS6XLp9R/logo.png"
+},
+{
+    name: "USDB",
+    address: "ACZDghSb25an1yfMQV3cdM5VvX9JDcUvePhksjCa92yf",
+    logo: "https://tokenlist.atcscan.io/metadata/ACZDghSb25an1yfMQV3cdM5VvX9JDcUvePhksjCa92yf/logo.png"
+},
+{
+    name: "BUSD",
+    address: "ACQLqcatoR3nKLJ3Hs8m7ydxHUib5BWiph7To2ATodhU",
+    logo: "https://tokenlist.atcscan.io/metadata/ACQLqcatoR3nKLJ3Hs8m7ydxHUib5BWiph7To2ATodhU/logo.png"
+},
+{
+    name: "GJ11",
+    address: "AC9RwDs7HoV65NFd5QtnkUPwrWTnANktAcoak33Qv4xv",
+    logo: "https://tokenlist.atcscan.io/metadata/AC9RwDs7HoV65NFd5QtnkUPwrWTnANktAcoak33Qv4xv/logo.png"
+},
+{
+    name: "LiuqiuCoin (LIUQIU)",
+    address: "ACj9UQvTi1D6pN7X6Dnmcgzpsnam9Ab5ikZ8LAVLVSkv",
+    logo: "https://tokenlist.atcscan.io/metadata/ACj9UQvTi1D6pN7X6Dnmcgzpsnam9Ab5ikZ8LAVLVSkv/logo.png"
+},
+{
+    name: "WIF",
+    address: "ACD3uWgLTWCyvaKfYyCKhdUB3FTLUGtgyyVrGjACVq1Y",
+    logo: "https://tokenlist.atcscan.io/metadata/ACD3uWgLTWCyvaKfYyCKhdUB3FTLUGtgyyVrGjACVq1Y/logo.png"
+},
+{
+    name: "POPCAT",
+    address: "ACygPor7YK4NJcEpoyigr6qkM8B5SEGTVnc8YQLC9iMY",
+    logo: "https://tokenlist.atcscan.io/metadata/ACygPor7YK4NJcEpoyigr6qkM8B5SEGTVnc8YQLC9iMY/logo.png"
+},
+{
+    name: "SHIB",
+    address: "ACFBDRVWMRvp9iYCrAVWbGfzr9tE5ddSapaoUugcKay3",
+    logo: "https://tokenlist.atcscan.io/metadata/ACFBDRVWMRvp9iYCrAVWbGfzr9tE5ddSapaoUugcKay3/logo.png"
+},
+{
+    name: "WEN",
+    address: "ACSghPtkMLkZ7qP1Bw5nbFKhV8VtvfNaWDjbEoyo2XWt",
+    logo: "https://tokenlist.atcscan.io/metadata/ACSghPtkMLkZ7qP1Bw5nbFKhV8VtvfNaWDjbEoyo2XWt/logo.png"
+},
+{
+    name: "Ponke (PONKE)",
+    address: "AChYBCQ2TmwC9YHtmqYDzb81u8pack4xHWNChZUD45Py",
+    logo: "https://tokenlist.atcscan.io/metadata/AChYBCQ2TmwC9YHtmqYDzb81u8pack4xHWNChZUD45Py/logo.png"
+},
+{
+    name: "Pudgy Penguins (PENGU)",
+    address: "ACUyHZC3C7H1Fh2153Zsw3Nb4zMaU8WzcotE7Wy7c8PJ",
+    logo: "https://tokenlist.atcscan.io/metadata/ACUyHZC3C7H1Fh2153Zsw3Nb4zMaU8WzcotE7Wy7c8PJ/logo.png"
+},
+{
+    name: "Mog Coin (MOG)",
+    address: "ACDzGKPYEy9HG86iQH6Ye5PxHw4iKjpCgxDuw6rntBpz",
+    logo: "https://tokenlist.atcscan.io/metadata/ACDzGKPYEy9HG86iQH6Ye5PxHw4iKjpCgxDuw6rntBpz/logo.png"
+},
+{
+    name: "Cat in a Dogs World (MEW)",
+    address: "ACuqDwT9BxAFLqvU1NTLN9Tag1oVDsWhvvCpvRZNpukN",
+    logo: "https://tokenlist.atcscan.io/metadata/ACuqDwT9BxAFLqvU1NTLN9Tag1oVDsWhvvCpvRZNpukN/logo.png"
+},
+{
+    name: "Potsdam Declaration (POTSDAM)",
+    address: "AC4cDvBG1mmSgK9Ntv31NYBP6m64VxCxzF5MtnWgkUWF",
+    logo: "https://tokenlist.atcscan.io/metadata/AC4cDvBG1mmSgK9Ntv31NYBP6m64VxCxzF5MtnWgkUWF/logo.png"
+},
+{
+    name: "GoldenGoose Coin (GOLD)",
+    address: "ACSPL8UeoW9xzE4EThQRAuGGTTzXp93k74Uv51zYwDbg",
+    logo: "https://tokenlist.atcscan.io/metadata/ACSPL8UeoW9xzE4EThQRAuGGTTzXp93k74Uv51zYwDbg/logo.png"
+},
+{
+    name: "Gigachad (GIGA)",
+    address: "ACDtLDPG58KjXFxHnnvWh3kLGQicLc8HXoH11DzYgDYQ",
+    logo: "https://tokenlist.atcscan.io/metadata/ACDtLDPG58KjXFxHnnvWh3kLGQicLc8HXoH11DzYgDYQ/logo.png"
+},
+{
+    name: "Floki Inu (FLOKI)",
+    address: "ACFbbZmrhuaUiREB4Nn8RjnnfZQnwPwy4ohDikb1fimX",
+    logo: "https://tokenlist.atcscan.io/metadata/ACFbbZmrhuaUiREB4Nn8RjnnfZQnwPwy4ohDikb1fimX/logo.png"
+},
+{
+    name: "Fartcoin (FARTCOIN)",
+    address: "ACg9H7rfba8UGdi2PMzcBeEQShRRSKP4KLmcRh5tbDxH",
+    logo: "https://tokenlist.atcscan.io/metadata/ACg9H7rfba8UGdi2PMzcBeEQShRRSKP4KLmcRh5tbDxH/logo.png"
+},
+{
+    name: "Dogelon Mars (ELON)",
+    address: "ACtTtKHDHtThjfM1wP4JooZ73Sc6nxZhGXaxB1cGEMBJ",
+    logo: "https://tokenlist.atcscan.io/metadata/ACtTtKHDHtThjfM1wP4JooZ73Sc6nxZhGXaxB1cGEMBJ/logo.png"
+},
+{
+    name: "Cheems (CHEEMS)",
+    address: "ACPwgPCf9CQ2jwZLCDfURZUMDotg9WGpuBm5A2qC5gWs",
+    logo: "https://tokenlist.atcscan.io/metadata/ACPwgPCf9CQ2jwZLCDfURZUMDotg9WGpuBm5A2qC5gWs/logo.png"
+},
+{
+    name: "Dino (DINO)",
+    address: "ACGNoNv5ACUuGRoDchy8JsciZ2NdZnmwmLr1sm6zGrH7",
+    logo: "https://tokenlist.atcscan.io/metadata/ACGNoNv5ACUuGRoDchy8JsciZ2NdZnmwmLr1sm6zGrH7/logo.png"
+},
+{
+    name: "Simons Cat (CAT)",
+    address: "AC9WBrimDyrsd3tjisfXVkKo9afFyWuypuo5j87aBvP6",
+    logo: "https://tokenlist.atcscan.io/metadata/AC9WBrimDyrsd3tjisfXVkKo9afFyWuypuo5j87aBvP6/logo.png"
+},
+{
+    name: "Brett (BRETT)",
+    address: "ACFgp1YmRNWWWcqLH6PMj7gC2F3YAj1my4nCraG1wjGH",
+    logo: "https://tokenlist.atcscan.io/metadata/ACFgp1YmRNWWWcqLH6PMj7gC2F3YAj1my4nCraG1wjGH/logo.png"
+},
+{
+    name: "Bonk (BONK)",
+    address: "ACnFsThauYAscrsf7kAGxxckGb1NL2rN5JbaBtj9r3nz",
+    logo: "https://tokenlist.atcscan.io/metadata/ACnFsThauYAscrsf7kAGxxckGb1NL2rN5JbaBtj9r3nz/logo.png"
+},
+{
+    name: "Book of MEME (BOME)",
+    address: "ACCmcFUxzdRCe8MytWG9ge5TWFNdbHXDRobNhs2QseGH",
+    logo: "https://tokenlist.atcscan.io/metadata/ACCmcFUxzdRCe8MytWG9ge5TWFNdbHXDRobNhs2QseGH/logo.png"
+},
+{
+    name: "BankrCoin (BNKR)",
+    address: "ACLKYqsgkmaZXhxWGmcmbZy9wmXTfLq8562Ygq45Jd56",
+    logo: "https://tokenlist.atcscan.io/metadata/ACLKYqsgkmaZXhxWGmcmbZy9wmXTfLq8562Ygq45Jd56/logo.png"
+},
+{
+    name: "Bunny King (BUNY)",
+    address: "ACVG36Y6xHY6iU1veoRuZncEg7qm6eKCmEXm6CwzYEEp",
+    logo: "https://tokenlist.atcscan.io/metadata/ACVG36Y6xHY6iU1veoRuZncEg7qm6eKCmEXm6CwzYEEp/logo.png"
+},
+{
+    name: "Stellar (XLM)",
+    address: "ACvUUEqg3KJMFoikm3onEiho1oL78oLYvp5hyGHs6PjE",
+    logo: "https://tokenlist.atcscan.io/metadata/ACvUUEqg3KJMFoikm3onEiho1oL78oLYvp5hyGHs6PjE/logo.png"
+},
+{
+    name: "Wall Street ETF (WETF)",
+    address: "ACpLzMNqb1yPXfGqrnGTV7SjCtDsGsW6ST9WXvx9Cx5m",
+    logo: "https://tokenlist.atcscan.io/metadata/ACpLzMNqb1yPXfGqrnGTV7SjCtDsGsW6ST9WXvx9Cx5m/logo.jpg"
+},
+{
+    name: "Virtual Protocol (VIRTUAL)",
+    address: "ACPNtrhiQ86cGcfzdLaeMkSE6bNWQssf3ydya7mg9ndb",
+    logo: "https://tokenlist.atcscan.io/metadata/ACPNtrhiQ86cGcfzdLaeMkSE6bNWQssf3ydya7mg9ndb/logo.png"
+},
+{
+    name: "Uniswap (UNI)",
+    address: "AC8BBu4F2v5c5twVkR7JByTpma6nR1qX8rzUz2WSZZjn",
+    logo: "https://tokenlist.atcscan.io/metadata/AC8BBu4F2v5c5twVkR7JByTpma6nR1qX8rzUz2WSZZjn/logo.png"
+},
+{
+    name: "Matic Token (MATIC)",
+    address: "ACV9zpQqx3BgfqYdBaoWpAaoEFFU55HW6yFLU8iXzoRn",
+    logo: "https://tokenlist.atcscan.io/metadata/ACV9zpQqx3BgfqYdBaoWpAaoEFFU55HW6yFLU8iXzoRn/logo.png"
+},
+{
+    name: "Kaito (KAITO)",
+    address: "ACG1KK1y29SxkuzcfuzCU7aJouoRyizbE6HiGj1JmxvM",
+    logo: "https://tokenlist.atcscan.io/metadata/ACG1KK1y29SxkuzcfuzCU7aJouoRyizbE6HiGj1JmxvM/logo.png"
+},
+
+];
+
+function renderTokens() {
+    const list = document.getElementById("tokenList");
+
+    list.innerHTML = tokens.map(t => `
+        <div class="token-card">
+            <img src="${t.logo}" class="token-logo">
+            <div class="token-name">${t.name}</div>
+            <div class="contract">${t.address}</div>
+
+            <div class="button-row">
+                <button class="btn" onclick="copyAddress('${t.address}')">Copy</button>
+                <button class="btn" onclick="window.open('https://atcscan.io/address/${t.address}', '_blank')">ATCScan</button>
+            </div>
+        </div>
+    `).join("");
+}
+
+function copyAddress(addr) {
+    navigator.clipboard.writeText(addr);
+    showCopied();
+}
+
+function showCopied() {
+    let alertBox = document.querySelector(".copy-alert");
+
+    if (!alertBox) {
+        alertBox = document.createElement("div");
+        alertBox.className = "copy-alert";
+        alertBox.textContent = "Copied!";
+        document.body.appendChild(alertBox);
+    }
+
+    alertBox.classList.add("show");
+
+    setTimeout(() => alertBox.classList.remove("show"), 1500);
+}
+
+// Load tokens
+renderTokens();
