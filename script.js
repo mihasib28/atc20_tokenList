35d26749032f77011255866a94e986a1c0e6dedd
mihasib <mihasib123@gmail.com>
2025-11-19 21:05:58 +0600

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const tokens = [
 {
     name: "Atlantis Coin (ATC)",
     address: "",
-    logo: "../image/atclogo.png",
+    logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/22039.png",
     isMainToken: true,
     description: "Native Utility Token of AtlantisChain.",
     links: [
