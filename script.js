71329996efda14a4721d49b5df7a40b25524e7b1
mihasib <mihasib123@gmail.com>
2025-11-26 20:14:54 +0600

    initial commit

--- script.js
+++ script.js
@@ -353,6 +353,13 @@ const tokens = [
 
 ];
 
+const navbarToggle = document.querySelector(".navbar-toggle");
+const navbarMenu = document.querySelector(".navbar-menu");
+navbarToggle.addEventListener("click", () => {
+    navbarToggle.classList.toggle("active");
+    navbarMenu.classList.toggle("active");
+});
+
 
 
 // --- Insert search bar ---
